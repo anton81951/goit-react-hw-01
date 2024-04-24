1b9cb610dcd3beb13c1568e7d695063359c9a833
Anton81951 <antonbugaichuk@gmail.com>
2024-04-24 22:42:03 +0300

    1

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,27 +1,27 @@
 import clsx from "clsx";
 import styles from "./TransactionHistory.module.css";
 
-export default function TransactionHistory ({
-    items: { id, type, amount,currency },}) {
-    return(
-        <table>
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
 
-  <tbody>
-  {transactions.map((trans) => (
-        <tr key={id}>
-        <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
+export default function TransactionHistory({ items }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
         </tr>
-      ))}
-  </tbody>
-</table>
-    )
+      </thead>
+
+      <tbody>
+        {items.map((trans) => (
+          <tr key={trans.id}>
+            <td>{trans.type}</td>
+            <td>{trans.amount}</td>
+            <td>{trans.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
